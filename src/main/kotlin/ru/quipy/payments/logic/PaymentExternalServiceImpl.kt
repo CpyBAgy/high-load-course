b738b4ceb9fd2ebc70b436eda44f7dffcce4472c
Bashkatov Ivan <i.bashkatov@aq.ru>
2026-03-05 00:43:43 +0300

    т

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -105,7 +105,26 @@ class PaymentExternalSystemAdapterImpl(
 
     private suspend fun bankPayment(transactionId: UUID, paymentId: UUID, amount: Int, paymentStartedAt: Long) {
         val urlString = "http://$paymentProviderHostPort/external/process?serviceName=$serviceName&token=$token&accountName=$accountName&transactionId=$transactionId&paymentId=$paymentId&amount=$amount"
-        sendRequest(transactionId, paymentId, urlString, paymentStartedAt)
+        sendRequestWithRetry(transactionId, paymentId, urlString, paymentStartedAt)
+    }
+
+    private suspend fun sendRequestWithRetry(
+        transactionId: UUID,
+        paymentId: UUID,
+        url: String,
+        paymentStartedAt: Long,
+    ) {
+        val delayMs = 100L
+        val maxRetries = 3
+        var curRetry = 1
+
+        while (curRetry < maxRetries) {
+            if (sendRequest(transactionId, paymentId, url, paymentStartedAt)) {
+                return
+            }
+            delay(delayMs)
+            curRetry += 1
+        }
     }
 
     suspend fun sendRequest(
