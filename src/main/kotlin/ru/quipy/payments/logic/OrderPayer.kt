ee6facedb5fe6b1685e6ffd2dc7f697f0c5c8365
Bashkatov Ivan <i.bashkatov@aq.ru>
2026-03-05 01:07:28 +0300

    в

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -27,29 +27,21 @@ class OrderPayer {
     private lateinit var paymentService: PaymentService
 
     private val paymentExecutor = ThreadPoolExecutor(
-        20,
-        20,
+        256,
+        256,
         0L,
         TimeUnit.MILLISECONDS,
-        LinkedBlockingQueue(10_000),
+        LinkedBlockingQueue(100_000),
         NamedThreadFactory("payment-submission-executor"),
-        ThreadPoolExecutor.DiscardOldestPolicy()
+        ThreadPoolExecutor.CallerRunsPolicy()
     )
 
     fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long {
         val createdAt = System.currentTimeMillis()
-        if (paymentExecutor.queue.size > 5000) {
-            return -1
-        }
         paymentExecutor.submit {
-            val createdEvent = paymentESService.create {
-                it.create(
-                    paymentId,
-                    orderId,
-                    amount
-                )
+            paymentESService.create {
+                it.create(paymentId, orderId, amount)
             }
-            logger.trace("Payment ${createdEvent.paymentId} for order $orderId created.")
             paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
         }
         return createdAt
